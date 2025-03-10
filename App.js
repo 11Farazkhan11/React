5d8fb858215f240c67ed8cbdf7518ccdbeac7eba
Faraz Khan <0farazkhan2@gmail.com>
2025-03-10 18:17:32 +0530

    React

--- App.js
+++ App.js
@@ -1,5 +1,21 @@
+/*
 const heading=React.createElement("h1",{},"hello kaise ho");
                                  //tag,obj,content
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
+*/
+const parent=React.createElement("div",{id:parent},[
+    Reat.createElement("div",{id:child1},[
+        React.createElement("h1",{},"hello h1"),
+        React.createElement("h1",{},"hello h2")
+    ]),
+    Reat.createElement("div",{id:child2},[
+        React.createElement("h1",{},"hello h1"),
+        React.createElement("h1",{},"hello h2")
+    ])
+]);
+
+const root=ReactDOM.createRoot(createElementById(parent));
+
+root.render(parent);
